c4531b4e8fc20355928f6c28f977dd1b39c2e332
Felix <fixone@users.noreply.github.com>
2019-12-07 11:43:57 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-8-jdk
 
-SPARK_VER="2.3.3"
+SPARK_VER="2.3.4"
 HADOOP_VER="2.7"
 
-DOWNLOADURL="https://www-eu.apache.org/dist/spark/spark-${SPARK_VER}/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz"
+DOWNLOADURL="https://archive.apache.org/dist/spark/spark-${SPARK_VER}/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz"
 echo "Downloading Apache Spark ${SPARK_VER}..."
 sudo wget -q -O /opt/spark.tgz ${DOWNLOADURL}
 echo "Unpacking Spark into /opt/spark"
