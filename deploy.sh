c07c0592a8438f0f6110a1327ba53a575270b9da
Felix <fixone@users.noreply.github.com>
2019-12-07 13:06:46 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
-echo "Installing Java (OpenJDK flavor)..."
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
+
+options=$(getopt -l "java,master,slave:" -o "jms:" -a -- "$@")
+eval set -- "$options"
+
+while true
+do
+  case $1 in
+    -j|--java)
+        export java=yes
+        ;;
+    -m|--master)
+        export master=yes
+        ;;
+    -s|--slaveof)
+        shift
+        export masterurl=$1
+        ;;
+    --)
+        shift
+        break;;
+  esac
+  shift
+done
+
+if [ "$java" = "yes" ]; then
+  echo "Installing Java (OpenJDK flavor)..."
+  sudo apt-get update
+  sudo apt-get install -y openjdk-8-jdk
+fi
 
 SPARK_VER="2.3.4"
 HADOOP_VER="2.7"
@@ -13,16 +39,23 @@ echo "Unpacking Spark into /opt"
 sudo tar -xzf /opt/spark.tgz --directory /opt
 echo "Making Spark available in /opt/spark"
 sudo ln -s /opt/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER} /opt/spark
-echo "Starting Spark Master ..."
-sudo /opt/spark/sbin/start-master.sh 
 
-MEM=$(grep MemTotal /proc/meminfo | awk '{print int($2 * 0.66 / 1024) }')
-CORES=$(grep -Pc '^processor\t' /proc/cpuinfo)
-MASTER=$(hostname --ip-address)
-MASTER="spark://${MASTER}:7077"
+if [ "$master" = "yes" ]; then
+  echo "Starting Spark Master ..."
+  sudo /opt/spark/sbin/start-master.sh
+  #point the slave to this master
+  masterurl=$(hostname --ip-address)
+  masterurl="spark://${masterurl}:7077"
+fi
 
-echo "Starting a Worker with ${MEM} MB (66% of total memory), ${CORES} cores and master ${MASTER}"
+if [ ! -z "$masterurl" ];
+then
+  MEM=$(grep MemTotal /proc/meminfo | awk '{print int($2 * 0.66 / 1024) }')
+  CORES=$(grep -Pc '^processor\t' /proc/cpuinfo)
+  echo "Starting a Worker with ${MEM} MB (66% of total memory), ${CORES} cores and master ${masterurl}"
+  sudo /opt/spark/sbin/start-slave.sh -c ${CORES} -m ${MEM}M ${masterurl}
+fi
 
-sudo /opt/spark/sbin/start-slave.sh -c ${CORES} -m ${MEM}M ${MASTER}
+
 
 
