52afe8c5e4936a2b1aa2c027d08260f91e37ada3
Felix <fixone@users.noreply.github.com>
2019-12-07 14:12:52 +0200

    Update gcloud-spark-master.sh

--- gcloud-spark-master.sh
+++ gcloud-spark-master.sh
@@ -7,6 +7,12 @@ then
         NODE=$1
 fi
 
+
+if [ $# -ge 2 ]
+then
+        MACHINE="n1-standard-$2"
+fi
+
 echo "using ${NODE} as instance name"
 
 gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts" --boot-disk-size "40" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "sparkmaster=true"
