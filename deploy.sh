4f8c8530ce7fa182f409548a56fafdb68ab9952c
Felix <fixone@users.noreply.github.com>
2019-12-07 11:19:58 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y openjdk-8-jdk
 
 SPARK_VER="2.3.3"
+HADOOP_VER="2.7"
 
-DOWNLOADURL="https://www-eu.apache.org/dist/spark/spark-${SPARK_VER}/spark-${SPARK_VER}-bin-hadoop2.7.tgz"
+DOWNLOADURL="https://www-eu.apache.org/dist/spark/spark-${SPARK_VER}/spark-${SPARK_VER}-bin-hadoop${HADOOP_VER}.tgz"
+
+sudo wget -q -O /opt/spark.tgz ${DOWNLOADURL}
+
+sudo tar -xzf /opt/spark.tgz
+
+
