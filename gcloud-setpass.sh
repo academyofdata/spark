90edba1e759aa883c63e94e9180f2f9edef50a86
Felix <fixone@users.noreply.github.com>
2019-12-07 21:41:35 +0200

    Update gcloud-setpass.sh

--- gcloud-setpass.sh
+++ gcloud-setpass.sh
@@ -6,5 +6,5 @@ NODES=$(gcloud compute instances list --format="csv[no-heading](name,zone)" --fi
 for N in ${NODES}
 do
   echo "running with ${N}"
-  awk -F, '{print "gcloud compute ssh " $1 " --zone " $2 " --command \"wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/config-setpass.sh | bash -s -- $1 $2 \" "}'
+  echo ${N} | awk -F, '{print "gcloud compute ssh " $1 " --zone " $2 " --command \"wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/config-setpass.sh | bash -s -- $1 $2 \""}' | bash -s -- $1 $2
 done
