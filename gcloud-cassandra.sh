aa5d0255ace286bcffe9bf8916acc7e392d9e909
Felix <fixone@users.noreply.github.com>
2019-12-07 14:33:09 +0200

    Update gcloud-cassandra.sh

--- gcloud-cassandra.sh
+++ gcloud-cassandra.sh
@@ -1,4 +1,5 @@
+ZONE="europe-west1-d"
 echo "finding internal address of the spark master (we deploy cassandra on the same node)"
 NODE=$(gcloud compute instances list --filter="labels.sparkmaster=true" --format="value(name)")
 echo "will install cassandra on ${NODE}"
-gcloud compute ssh 
+gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- "
