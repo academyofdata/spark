adb49a5f1d3e0eccdd8d22c95c30ce11a6f8d6d7
Felix <fixone@users.noreply.github.com>
2019-12-07 15:10:53 +0200

    Update gcloud-setpass.sh

--- gcloud-setpass.sh
+++ gcloud-setpass.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 LABEL="aodcluster"
 
-NODES=$(gcloud compute instances list --format="value(name)" --filter="status=RUNNING AND labels.${LABEL}=true")
+NODES=$(gcloud compute instances list --format="value(zone,name)" --filter="status=RUNNING AND labels.${LABEL}=true")
 
 for N in ${NODES}
 do
   echo "running on ${N}"
-  gcloud compute ssh ${N} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/config-setpass.sh | bash -s -- $1 $2"
+  #gcloud compute ssh ${N} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/config-setpass.sh | bash -s -- $1 $2"
 done
