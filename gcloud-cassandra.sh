07d146aef14a30e0e8228f9c73fbfca1db6fefe9
felix <felix@netopia.ro>
2019-12-08 18:42:52 +0200

    label before ssh

--- gcloud-cassandra.sh
+++ gcloud-cassandra.sh
@@ -2,6 +2,5 @@ ZONE="europe-west1-d"
 echo "finding internal address of the spark master (we deploy cassandra on the same node)"
 NODE=$(gcloud compute instances list --filter="labels.sparkmaster=true" --format="value(name)")
 echo "will install cassandra on ${NODE}"
-gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy-cassandra.sh | bash"
-
 gcloud compute instances add-labels ${NODE} --zone ${ZONE} --labels=cassandra=true
+gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy-cassandra.sh | bash"
