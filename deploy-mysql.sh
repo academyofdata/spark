d9352444b54fe56b6a2a2ba537c3fb5bb2ecc7ad
felix <felix@netopia.ro>
2019-12-08 16:51:22 +0200

    set-selections

--- deploy-mysql.sh
+++ deploy-mysql.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 sudo apt-get update
-export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y mysql-server
+#export DEBIAN_FRONTEND=noninteractive
 if [ $# -ge 1 ]
 then
     password=$1
 else
     password="mysql!P@ss"
 fi
-mysqladmin -u root password ${password}
+#mysqladmin -u root password ${password}
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${password}"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${password}"
+
+sudo apt-get install -y mysql-server
