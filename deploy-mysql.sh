6f1733f8a9dabd7e16ad99f84cae0eb38dc1cfa7
felix <felix@netopia.ro>
2019-12-08 17:05:51 +0200

    sel + noninteractive

--- deploy-mysql.sh
+++ deploy-mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-#export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 if [ $# -ge 1 ]
 then
     password=$1
@@ -8,6 +8,7 @@ else
     password="mysql!P@ss"
 fi
 #mysqladmin -u root password ${password}
+echo "Setting root password to ${password}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password ${password}"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${password}"
 
