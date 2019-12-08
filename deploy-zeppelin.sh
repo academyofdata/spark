2ad494e760ca53a7a8bed5448e498467ff3230a4
felix <felix@netopia.ro>
2019-12-08 11:38:07 +0200

    with sudo

--- deploy-zeppelin.sh
+++ deploy-zeppelin.sh
@@ -31,23 +31,23 @@ else
 	PASSWORD='zeplnp@ss'
 fi
 
-echo "getting Zeppeling Archive"
-wget -q O /opt/zeppelin.tgz http://apache.javapipe.com/zeppelin/zeppelin-${ZEP_VER}/zeppelin-${ZEP_VER}-bin-all.tgz
+echo "getting Zeppelin Archive"
+sudo wget -q O /opt/zeppelin.tgz http://apache.javapipe.com/zeppelin/zeppelin-${ZEP_VER}/zeppelin-${ZEP_VER}-bin-all.tgz
 echo "unpacking..."
-tar -xzvf /opt/zeppelin.tgz
+sudo tar -xzvf /opt/zeppelin.tgz
 #enable authentication
-cp /opt/zeppelin/conf/shiro.ini.template /opt/zeppelin/conf/shiro.ini
+sudo cp /opt/zeppelin/conf/shiro.ini.template /opt/zeppelin/conf/shiro.ini
 #the Apache shiro template comes with a bunch of users pre-defined, remove them
-sed -i "/^user/d" /opt/zeppelin/conf/shiro.ini 
+sudo sed -i "/^user/d" /opt/zeppelin/conf/shiro.ini 
 # admin default password in shiro.ini is password1, change it to a value of our own
-sed -i "s/password1/${PASSWORD}/g" /opt/zeppelin/conf/shiro.ini
+sudo sed -i "s/password1/${PASSWORD}/g" /opt/zeppelin/conf/shiro.ini
 
-cp /opt/zeppelin/conf/zeppelin-site.xml.template /opt/zeppelin/conf/zeppelin-site.xml
+sudo cp /opt/zeppelin/conf/zeppelin-site.xml.template /opt/zeppelin/conf/zeppelin-site.xml
 #disable anonymous access
-sed -i '/zeppelin.anonymous.allowed/{n;s/.*/<value>false<\/value>/}' /opt/zeppelin/conf/zeppelin-site.xml
+sudo sed -i '/zeppelin.anonymous.allowed/{n;s/.*/<value>false<\/value>/}' /opt/zeppelin/conf/zeppelin-site.xml
 
 echo "starting daemon..."
-/opt/zeppelin/bin/zeppelin-daemon.sh start
+sudo /opt/zeppelin/bin/zeppelin-daemon.sh start
 
 
 if [ ! -z "$cassandra" ]
@@ -55,10 +55,10 @@ then
 	echo "waiting for Zeppelin to start to set Cassandra host and dependencies..."
 	#wait untin Zeppelin starts and creates the interpreter.json file
 	sleep 45
-	sed -i "s/\"cassandra.hosts\": \"localhost\"/\"cassandra.hosts\": \"${cassandra}\"/g" /opt/zeppelin/conf/interpreter.json
-	sed -i "s/\"cassandra.cluster\": \"Test Cluster\"/\"cassandra.cluster\": \"CassandraTraining\"/g" /opt/zeppelin/conf/interpreter.json
-	sed -i "s/\"spark.cores.max\": \"\"/\"spark.cores.max\": \"\",\"spark.cassandra.connection.host\": \"${cassandra}}\"/g" /opt/zeppelin/conf/interpreter.json
-	echo "re-starting daemon..."
+	sudo sed -i "s/\"cassandra.hosts\": \"localhost\"/\"cassandra.hosts\": \"${cassandra}\"/g" /opt/zeppelin/conf/interpreter.json
+	sudo sed -i "s/\"cassandra.cluster\": \"Test Cluster\"/\"cassandra.cluster\": \"CassandraTraining\"/g" /opt/zeppelin/conf/interpreter.json
+	sudo sed -i "s/\"spark.cores.max\": \"\"/\"spark.cores.max\": \"\",\"spark.cassandra.connection.host\": \"${cassandra}}\"/g" /opt/zeppelin/conf/interpreter.json
+	sudo echo "re-starting daemon..."
 	/opt/zeppelin/bin/zeppelin-daemon.sh restart
 
 fi
