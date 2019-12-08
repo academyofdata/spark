4abe16e0789698f92fb2b0c725d6a77c5cf622a9
felix <felix@netopia.ro>
2019-12-08 16:44:56 +0200

    noninteractive

--- deploy-mysql.sh
+++ deploy-mysql.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
 sudo apt-get install -y mysql-server
+if [ $# -ge 1 ]
+then
+    password=$1
+else
+    password="mysql!P@ss"
+fi
+mysqladmin -u root password ${password}
