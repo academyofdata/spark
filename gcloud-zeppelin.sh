6ce62041159e58d3a768521c80e2e326b04acf95
felix <felix@netopia.ro>
2019-12-08 18:00:49 +0200

    master with scheme

--- gcloud-zeppelin.sh
+++ gcloud-zeppelin.sh
@@ -2,7 +2,8 @@
 ZONE="europe-west1-d"
 MASTER=$(gcloud compute instances list --filter="labels.sparkmaster=true" --format="value(name)")
 CASSANDRA=$(gcloud compute instances list --filter="labels.cassandra=true" --format="value(name)")
+MASTER="spark://${MASTER}:7077"
 
 
-echo "will install zeppelin with master ${MASTER}"
+echo "will install zeppelin with master ${MASTER} and cassandra node ${CASSANDRA} "
 gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy-zeppelin.sh | bash -s -- -m ${MASTER} -c ${CASSANDRA} -s zeplpassw!"
