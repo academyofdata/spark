28ab60d8f540be220c5a286a6b5a4d087cf60463
Felix <fixone@users.noreply.github.com>
2019-12-08 11:14:53 +0200

    Update gcloud-spark-slave.sh

--- gcloud-spark-slave.sh
+++ gcloud-spark-slave.sh
@@ -23,4 +23,4 @@ MASTERIP=$(gcloud compute instances list --filter="labels.sparkmaster=true" --fo
 echo "master IP is ${MASTERIP}"
 
 echo "installing and configuring Spark on remote node"
-gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy.sh | bash -s -- --java --slaveof spark://${MASTERIP}:7077"
+gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy-spark.sh | bash -s -- --java --slaveof spark://${MASTERIP}:7077"
