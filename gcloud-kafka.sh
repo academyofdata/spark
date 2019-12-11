7fcf67f42e691f74fe877f56419569369737d2eb
felix <felix@netopia.ro>
2019-12-11 09:56:29 +0200

    gcloud for kafka

---
+++ gcloud-kafka.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ZONE="europe-west1-d"
+#we install on the same machine as spark master
+NODE=$(gcloud compute instances list --filter="labels.sparkmaster=true" --format="value(name)")
+
+gcloud compute instances add-labels ${NODE} --zone ${ZONE} --labels=kafkabroker=true,zookeeper=true
+
+echo "will install kafka with on ${NODE}"
+gcloud compute ssh ${NODE} --zone ${ZONE} --command "wget -qO- https://raw.githubusercontent.com/academyofdata/spark/master/deploy-kafka.sh | bash"
