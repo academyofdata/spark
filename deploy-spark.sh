f4fb06dfb415fe10baaf7a73011e3770edc059aa
Felix <fixone@users.noreply.github.com>
2020-01-31 10:56:57 +0200

    Update deploy-spark.sh

--- deploy-spark.sh
+++ deploy-spark.sh
@@ -59,7 +59,7 @@ then
   echo "Starting a Worker with ${MEM} MB (${MEMFACTOR} of total memory), ${CORES} cores and master ${masterurl}"
   sudo /opt/spark/sbin/start-slave.sh -c ${CORES} -m ${MEM}M ${masterurl}
   #executor settings valid for e.g. spark-shell on this machine
-  sudo echo "spark master ${masterurl}" >> /opt/spark/conf/spark-defaults.conf
+  sudo echo "spark.master ${masterurl}" >> /opt/spark/conf/spark-defaults.conf
   sudo echo "spark.executor.memory 1G" >> /opt/spark/conf/spark-defaults.conf
   sudo echo "spark.executor.cores 1" >> /opt/spark/conf/spark-defaults.conf
  
